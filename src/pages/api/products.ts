d5d86ea8954a85f3054bf152873d2e7f8b6218aa
yttiiz <dominique.talis@proton.me>
2025-11-12 15:22:27 -0400

    chore(products): remove product fetching API and related code

--- src/pages/api/products.ts
+++
@@ -1,9 +0,0 @@
-import { getProductsService } from "@services";
-import type { APIRoute } from "astro";
-
-export const GET: APIRoute = async ({ request }) => {
-	const params = request.url.split("?")[1];
-	const data = await getProductsService(params);
-
-	return new Response(JSON.stringify(data));
-};
--- src/sdk/fetcher.ts
+++ src/sdk/fetcher.ts
@@ -5,7 +5,6 @@ import type {
 	BookingsType,
 	ContactMessageType,
 	ItemType,
-	ProductCardContentType,
 	ProductSchemaWithIDType,
 	ReviewsProductSchemaWithIDType,
 	UserBookingsType,
@@ -38,41 +37,6 @@ export const getMenuData = (menuItems: ItemType[] | undefined) => {
 	);
 
 	return menuItems;
-};
-
-export const getProducts = async () => {
-	type ProductRefinedReviewType = (ProductSchemaWithIDType & {
-		review: ReviewsProductSchemaWithIDType;
-	})[];
-
-	const products: ProductCardContentType[] = [];
-	const res = await Fetcher.getData<ProductRefinedReviewType>(
-		getUrl("/api/products?withReview"),
-	);
-
-	if (res.ok) {
-		for (const {
-			details,
-			description,
-			name,
-			thumbnail,
-			pictures,
-			_id,
-			review,
-		} of res.data) {
-			products.push({
-				details,
-				description,
-				href: `/product/${_id.toString()}`,
-				name: `Aka ${name}`,
-				thumbnail,
-				pictures,
-				review,
-			});
-		}
-	}
-
-	return products;
 };
 
 export const getProduct = async (id: string) => {
